83df9b3ec9fff6cb31314a767a2cc9683ea4f59b
Fabian Gaspero-Beckstrom <fasogabe@Loki.local>
2021-03-04 11:24:08 -0800

    add photos to static assets

---
+++ gallery.js
@@ -0,0 +1,9 @@
+const gallery = [
+  {
+    card_id: 'panel-1',
+    thumbnail: 'cedarsaint_deck/IMG_0257.jpeg',
+    title: 'Cedarsaint Deck',
+    description: 'This is our cedarsaint deck project.',
+    modal_id: 'modal-1',
+  },
+];
---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('navbar').style.top = '0';
+  } else {
+    document.getElementById('navbar').style.top = '-86px';
+  }
+  prevScrollpos = currentScrollPos;
+};
