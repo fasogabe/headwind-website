4e766c8fe9474c08f4af9c7943be1b6aac7e5ce9
Fabian Gaspero-Beckstrom <fasogabe@Loki.local>
2021-03-08 18:44:29 -0800

    changed styles/added team photos

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,4 +1,4 @@
-const contact_section = document.querySelector('.contact');
+const contact_container = document.getElementById('contact-container');
 const contact_form = document.getElementById('contact-form');
 
 contact_form.addEventListener('submit', (e) => {
@@ -17,7 +17,10 @@ const sendMail = (mail) => {
         console.log(`Error - Status Code: ${response.status}`);
         insertAlert('danger', 'Error: Email not sent.');
       } else {
-        insertAlert('success', 'Email has been sent!');
+        insertAlert(
+          'success',
+          'Email has been sent! Thank you for your message. We will respond as soon as possible.'
+        );
       }
     })
     .catch((err) => console.log(`Fetch error: ${err}`));
@@ -26,9 +29,19 @@ const sendMail = (mail) => {
 const insertAlert = (type, msg) => {
   const alert = document.createElement('div');
   alert.innerHTML = `
-        <div class="alert alert-${type}" role="alert">
+        <div id="contact-alert" class="alert alert-${type}" role="alert">
           ${msg}
         </div>
       `;
-  contact_section.insertBefore(alert, contact_form);
+  contact_container.insertBefore(alert, contact_form);
+  clearFields();
+  setTimeout(() => {
+    document.getElementById('contact-alert').remove();
+  }, 5000);
+};
+
+const clearFields = () => {
+  const name = (document.getElementById('contactFormName').value = '');
+  const email = (document.getElementById('contactFormEmail').value = '');
+  const message = (document.getElementById('contactFormMessage').value = '');
 };
